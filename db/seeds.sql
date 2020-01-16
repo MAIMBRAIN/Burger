8401e9f4e0c17f3e0e7938798752ab268ee2c708
Avery Stahl <averystahl@hotmail.com>
2020-01-16 11:01:49 -0500

    Created database, populated db with data

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Wake-Up Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Ham Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossibly Possible Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portabello Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheddar Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Meatball Burger", 0);
