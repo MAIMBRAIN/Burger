3fc80ee3fd602b24e09c7bcce936ebf060b9af0b
Avery Stahl <averystahl@hotmail.com>
2020-02-11 09:33:28 -0500

    burgers show up without name

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,4 @@
-const burger = require("..burger");
+const burger = require("../models/burger");
 const express = require("express");
 
 const router = express.Router();
@@ -7,10 +7,11 @@ router.get("/", function(req, res)
 {
     burger.selectAll(function(data)
     {
-        let hbsObject = {cats: data};
+        let hbsObject = {burgers: data};
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
+    
 });
 
 router.post("/api/burgers", function(req, res)
@@ -35,6 +36,27 @@ router.put("/api/burgers/:id", function(req, res)
         if (result.changedRows === 0)
         {
             return res.status(404).end();
+        }
+        else
+        {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res)
+{
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result)
+    {
+        if (result.affectedRows == 0)
+        {
+            return res.status(404).end();
+        }
+        else
+        {
+            res.status(200).end();
         }
     });
 });
